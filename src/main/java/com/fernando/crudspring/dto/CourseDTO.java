693af37269d22e96265b93529318041d039a0290
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-04-22 10:27:00 -0300

    CourseDTO criado

---
+++ src/main/java/com/fernando/crudspring/dto/CourseDTO.java
@@ -0,0 +1,17 @@
+package com.fernando.crudspring.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO (
+	@JsonProperty("_id") Long id,
+	@NotBlank @NotNull @Length(min = 5, max = 100) String name,
+	@NotBlank @NotNull @Length(max = 10) @Pattern(regexp = "back-end|front-end") String category
+	) {
+}
+
