d8021edcedc2b5a0886b069fd27df1214440699b
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-01-27 14:13:00 -0300

    removidos imports desnecessários

--- src/main/java/dev/fernando/crudspring/controller/CoursesController.java
+++ src/main/java/dev/fernando/crudspring/controller/CoursesController.java
@@ -1,14 +1,6 @@
 package dev.fernando.crudspring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import dev.fernando.crudspring.model.Course;
-import dev.fernando.crudspring.repository.CourseRepository;
-import lombok.AllArgsConstructor;
-
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +9,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.fernando.crudspring.model.Course;
+import dev.fernando.crudspring.repository.CourseRepository;
+import lombok.AllArgsConstructor;
 
 
 
