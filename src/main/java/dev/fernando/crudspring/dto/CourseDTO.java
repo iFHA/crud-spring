f4ca4c0e7f49fbe556e14c085ec454e2de4b463f
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-02-03 00:41:05 -0300

    corrigido bug do id

--- src/main/java/dev/fernando/crudspring/dto/CourseDTO.java
+++ src/main/java/dev/fernando/crudspring/dto/CourseDTO.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
-	@NotNull
 	Long _id,
 	
 	@NotBlank
